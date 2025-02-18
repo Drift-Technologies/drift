fbac7d6e4dea50d91f9fc2c02b42fd492f2fbb62
Yibin Long <yibinlong@outlook.com>
2025-02-18 13:54:26 -0800

    feat: add wallet to bottom tab

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -45,6 +45,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="creditcard.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="wallet"
+        options={{
+          title: 'Wallet',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="creditcard.fill" color={color} />,
+        }}
+      />
     </Tabs>
     </ParamsProvider>
   );
