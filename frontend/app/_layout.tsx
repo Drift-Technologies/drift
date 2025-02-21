71369835edb4932bdf6dfdd71799268a4e3293c2
Yibin Long <yibinlong@outlook.com>
2025-02-21 13:48:18 -0800

    feat: add card details to layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout() {
           <Stack.Screen name="index" />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="ride-details" options={{ headerShown: false }} />
+          <Stack.Screen name="card-details" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
