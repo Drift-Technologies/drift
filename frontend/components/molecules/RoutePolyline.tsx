3b35dc43c50b62c4aad4bf8a4d0043732ac86c3c
farhan64 <farhan64@student.ubc.ca>
2025-03-23 10:32:58 -0700

    fix: more varied color palette

--- frontend/components/molecules/RoutePolyline.tsx
+++ frontend/components/molecules/RoutePolyline.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Polyline } from 'react-native-maps';
 
+import {  get_color_from_route_id, } from '@/components/utils/route_utils';
+
 export const renderPolyline = (shapeIdStr: string, routes: any) => {
     const shapeId = parseInt(shapeIdStr, 10);
     const routePoints = routes[shapeId];
@@ -9,14 +11,12 @@ export const renderPolyline = (shapeIdStr: string, routes: any) => {
   
     // Extract route details from the first point
     const routeId = routePoints[0].route_id;
-    const routeColor = routePoints[0].color;
-    // console.log("Rendering polyline for shapeId:", shapeIdStr);
 
     return (
         <Polyline
             key={shapeId} // Use shape_id as key
             coordinates={routePoints.map(({ latitude, longitude }) => ({ latitude, longitude }))} // Extract coordinates
-            strokeColor={routeColor} // Use color from first point
+            strokeColor={get_color_from_route_id(routeId)} // Use color from first point
             strokeWidth={6}
         />
     );
