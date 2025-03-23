8859fcbe96ba19945f8574d8218b63352097e7c0
farhan64 <farhan64@student.ubc.ca>
2025-03-23 10:11:30 -0700

    fix dynamic border color of bus list + refactor into component

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from 'react';
 import {Platform, StatusBar } from 'react-native';
 
 import { ScrollView, View, Text} from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from '@/components/atoms/HelloWave';
+import { ThemedText } from '@/components/atoms/ThemedText';
+import { ThemedView } from '@/components/atoms/ThemedView';
 
 import VancouverMap from '@/components/organisms/VancouverMap'
 import { useParams } from '@/context/ParamsContext';
-import {getTripHeadsign} from '@/components/utils/route_utils';
+import {get_trip_head_sign} from '@/components/utils/route_utils';
 import { AnimatedRegion } from 'react-native-maps';
 import { HomeStyle } from '@/components/styles/HomePage.styles';
+
+
+import ClosestBusesList from '@/components/molecules/ClosestBusesList';
+
+
 
 export default function HomeScreen() {
   const statusBarHeight = Platform.OS === 'android' ? StatusBar.currentHeight || 0 : 0;
@@ -54,12 +59,8 @@ export default function HomeScreen() {
         <VancouverMap 
           location={location}
           setLocation={setLocation}
-          // routes={routes} 
-          // setRoutes={setRoutes}
           animatedBusData={animatedBusData}
           setAnimatedBusData={setAnimatedBusData}
-          // onBus={onBus}
-          // setOnBus={setOnBus}
           />
       </View>
 
@@ -78,26 +79,8 @@ export default function HomeScreen() {
       </View>
 
       {/* Closest Buses Section */}
-      <View style={HomeStyle.closestBusesContainer}>
-        <Text style={HomeStyle.closestBusesTitle}>Closest Bus Routes</Text>
-        {busesOnRoutes.length > 0 ? (
-          <View style={HomeStyle.cardsContainer}>
-            {busesOnRoutes.map((bus) => (
-              <View key={bus["route_id"]} style={[HomeStyle.card, { borderColor: "green", borderWidth: 3 }]}>
-                <Text style={HomeStyle.cardTitle}>
-                  Bus: {getTripHeadsign(bus["route_id"]).split(' ')[0] || 'Unknown'}
-                  {/* busNames.find((bus) => Number(bus.shape_id) === routeId)?.trip_headsign.split(' ')[0] || 'Unknown'}  */}
-                </Text>
-                <Text style={HomeStyle.cardDistance}>
-                  ({getTripHeadsign(bus["route_id"]) || 'Unknown'})
-                </Text>
-              </View>
-            ))}
-          </View>
-        ) : (
-          <Text style={HomeStyle.noBusesText}>No closest buses available at the moment!</Text>
-        )}
-      </View>
+      <ClosestBusesList buses={busesOnRoutes} />
+
     </ScrollView>
   );
 }
